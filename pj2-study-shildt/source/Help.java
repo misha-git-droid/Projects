cf490e2717ee46350aeb7b429e53af9976bf2f49
User Mikhail <budarin.flip@gmail.com>
2024-07-22 15:51:16 +0500

    скомпилировать help.class

--- pj2-study-shildt/source/Help.java
+++ pj2-study-shildt/source/Help.java
@@ -3,7 +3,7 @@
 */
 
 class Help {
-	public static void main (String [] args) {
+	public static void main (String [] args) 
 		throws java.io.IOException {
 		
 		char choice, ignore;
@@ -19,15 +19,41 @@ class Help {
 			choice = (char) System.in.read();
 			do {
 				ignore = (char) System.in.read();
-			} while (ignore != \n);
+			} while (ignore != '\n');
 		} while (choice < '1' | choice > '5');
 		
-		}
+		
 		
 		switch (choice) {
 			case '1':
-			System.out.println("Оператор if:\n");
-			System.out.println("if (условие) оерп");
+				System.out.println("Оператор if:\n");
+				System.out.println("if (условие) оператор;");
+				System.out.println("else оператор;");
+				break;
+			case '2':
+				System.out.println("Традиционный оператор switch:\n");
+				System.out.println("switch (выражение) {");
+				System.out.println("	case константа:");
+				System.out.println("		последовательность операторов");
+				System.out.println("		break;");
+				System.out.println("	// ...");
+				System.out.println("}");
+				break;
+			case '3':
+				System.out.println("Цикл for:\n");
+				System.out.println("for (инициализация; условие; итерация)");
+				System.out.println("	оператор;");
+				break;
+			case '4':
+				System.out.println("Цикл while:\n");
+				System.out.println("while (условие) оператор;");
+				break;
+			case '5':
+				System.out.println("Цикл do-while:\n");
+				System.out.println("do {");
+				System.out.println("	оператор;");
+				System.out.println("} while (условие);");
+				break;
 		}
 	}
 }
