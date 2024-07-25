76e883c68a0268729a087e98484dfadcb46671c9
User Mikhail <budarin.flip@gmail.com>
2024-07-25 14:16:20 +0500

    перевести проект с русского на английский

--- pj2-study-shildt/source/Help.java
+++ pj2-study-shildt/source/Help.java
@@ -14,7 +14,10 @@ class Help {
 			System.out.println("	3. for");
 			System.out.println("	4. while");
 			System.out.println("	5. do-while\n");
-			System.out.println("Select an option ");
+			System.out.println("	6. break");
+			System.out.println("	7. continue\n");
+			System.out.print("Select an option: ")
+			
 			
 			choice = (char) System.in.read();
 			do {
@@ -26,33 +29,33 @@ class Help {
 		
 		switch (choice) {
 			case '1':
-				System.out.println("Оператор if:\n");
-				System.out.println("if (условие) оператор;");
-				System.out.println("else оператор;");
+				System.out.println("Operator if:\n");
+				System.out.println("if (requirement) operator;");
+				System.out.println("else operator;");
 				break;
 			case '2':
-				System.out.println("Традиционный оператор switch:\n");
-				System.out.println("switch (выражение) {");
-				System.out.println("	case константа:");
-				System.out.println("		последовательность операторов");
+				System.out.println("Traditional operator switch:\n");
+				System.out.println("switch (expression) {");
+				System.out.println("	case constant:");
+				System.out.println("		a sequence of operators");
 				System.out.println("		break;");
 				System.out.println("	// ...");
 				System.out.println("}");
 				break;
 			case '3':
-				System.out.println("Цикл for:\n");
-				System.out.println("for (инициализация; условие; итерация)");
-				System.out.println("	оператор;");
+				System.out.println("Cycle for:\n");
+				System.out.println("for (initialization; requirement; iteration)");
+				System.out.println("	operator;");
 				break;
 			case '4':
-				System.out.println("Цикл while:\n");
-				System.out.println("while (условие) оператор;");
+				System.out.println("Cycle while:\n");
+				System.out.println("while (requirement) operator;");
 				break;
 			case '5':
-				System.out.println("Цикл do-while:\n");
+				System.out.println("Cycle do-while:\n");
 				System.out.println("do {");
-				System.out.println("	оператор;");
-				System.out.println("} while (условие);");
+				System.out.println("	operator;");
+				System.out.println("} while (requirement);");
 				break;
 		}
 	}
