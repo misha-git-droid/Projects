dcb205c198cf464103d77af1c4e34c5403bad24b
User Mikhail <budarin.flip@gmail.com>
2024-08-01 22:24:55 +0500

    добавить проект отображения числа типа байт в двоичном представлении

---
+++ interesting_projects/ShowBits/source/ShowBits.java
@@ -0,0 +1,15 @@
+/*
+	Displaying the bits of a byte type value
+*/
+
+class ShowBits {
+	public static void main (String [] args) {
+		int t;
+		byte val;
+		val = 123;
+		for (t = 128; t > 0; t /= 2) {
+			if ((val & t) != 0) System.out.print("1 ");
+			else System.out.print("0 ");
+		}
+	}
+}
