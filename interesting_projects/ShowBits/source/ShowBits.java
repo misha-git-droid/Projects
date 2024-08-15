ebe65bda36e3fb494a0e87065405f81bdaab7bd7
Mikhail-2426 <budarin.flip@gmail.com>
2024-08-15 14:46:37 +0300

    добавить перевода любого целочисленного числа в двоичное представление

--- interesting_projects/ShowBits/source/ShowBits.java
+++ interesting_projects/ShowBits/source/ShowBits.java
@@ -3,13 +3,28 @@
 */
 
 class ShowBits {
-	public static void main (String [] args) {
-		int t;
-		byte val;
-		val = 123;
-		for (t = 128; t > 0; t /= 2) {
-			if ((val & t) != 0) System.out.print("1 ");
-			else System.out.print("0 ");
+	
+	int numbits;
+	
+	ShowBits(int n) {
+		numbits = n;
+	}
+	
+	void show() {
+		long mask = 1;
+		
+		mask <<= numbits-1;
+		
+		int spacer = 0;
+		for (: mask != 0; mask >>>= 1) {
+			if ((val & mask) != 0) System.out.print("1");
+			else System.out.print("0");
+			spacer++;
+			if ((spacer % 8) == 0) {
+				System.out.print(" ");
+				spacer = 0;
+			}
 		}
+		System.out.println();
 	}
 }
